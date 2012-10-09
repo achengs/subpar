d28e3126d629cacceee6ec2b97ca51778ec0ff0d
Andrew Cheng <achengs@gmail.com>
2012-10-09 11:21:40 -0400

    renamed directories

--- project.clj
+++ project.clj
@@ -2,28 +2,23 @@
   :description "an approximate subset of paredit for codemirror"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
-                                        ; Enable the lein hooks for: clean, compile, test, and jar.
   :hooks [leiningen.cljsbuild]
   :cljsbuild {
               :test-commands
-                                        ; Test command for running the unit tests in "test-cljs" (see below).
+                                        ; Test command for running the unit tests in "test" (see below).
                                         ;     $ lein cljsbuild test
               {"unit" ["phantomjs"
                        "phantom/unit-test.js"
                        "resources/private/html/unit-test.html"]}
               :builds {
-                                        ; This build has the lowest level of optimizations, so it is
-                                        ; useful when debugging the app.
                        :dev
-                       {:source-path "src-cljs"
+                       {:source-path "src"
                         :jar true
                         :compiler {:output-to "resources/public/js/subpar.core.debug.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}
-                                        ; This build has the highest level of optimizations, so it is
-                                        ; efficient when running the app in production.
                        :prod
-                       {:source-path "src-cljs"
+                       {:source-path "src"
                         :compiler {:output-to "resources/public/js/subpar.core.js"
                                    :optimizations :advanced
                                    :pretty-print false}}
@@ -31,7 +26,7 @@
                                         ; be run via PhantomJS.  See the phantom/unit-test.js file
                                         ; for details on how it's run.
                        :test
-                       {:source-path "test-cljs"
+                       {:source-path "test"
                         :compiler {:output-to "resources/private/js/unit-test.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}}})

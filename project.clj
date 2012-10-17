d0dd58a686d8f937f8d705bdaa2447fdb14eb8ba
Andrew Cheng <achengs@gmail.com>
2012-10-17 15:27:47 -0400

    minor formatting

--- project.clj
+++ project.clj
@@ -3,33 +3,33 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :hooks [leiningen.cljsbuild]
-  :cljsbuild {
-              :test-commands
+  :cljsbuild
+  {:test-commands
                                         ; Test command for running the unit tests in "test" (see below).
                                         ;     $ lein cljsbuild test
-              {"unit" ["phantomjs"
-                       "phantom/unit-test.js"
-                       "resources/private/html/unit-test.html"]}
-              :builds {
-                       :dev
-                       {:source-path "src"
-                        :jar true
-                        :compiler {:output-to "resources/public/js/subpar.core.debug.js"
-                                   :optimizations :whitespace
-                                   :externs ["resources/private/js/codemirror-externs.js"]
-                                   :pretty-print true}}
-                       :prod
-                       {:source-path "src"
-                        :compiler {:output-to "resources/public/js/subpar.core.js"
-                                   :optimizations :advanced
-                                   :externs ["resources/private/js/codemirror-externs.js"]
-                                   :pretty-print false}}
+   {"unit" ["phantomjs"
+            "phantom/unit-test.js"
+            "resources/private/html/unit-test.html"]}
+   :builds {
+            :dev
+            {:source-path "src"
+             :jar true
+             :compiler {:output-to "resources/public/js/subpar.core.debug.js"
+                        :optimizations :whitespace
+                        :externs ["resources/private/js/codemirror-externs.js"]
+                        :pretty-print true}}
+            :prod
+            {:source-path "src"
+             :compiler {:output-to "resources/public/js/subpar.core.js"
+                        :optimizations :advanced
+                        :externs ["resources/private/js/codemirror-externs.js"]
+                        :pretty-print false}}
                                         ; This build is for the ClojureScript unit tests that will
                                         ; be run via PhantomJS.  See the phantom/unit-test.js file
-                                        ; for details on how it's run.
-                       :test
-                       {:source-path "test"
-                        :compiler {:output-to "resources/private/js/unit-test.js"
-                                   :optimizations :whitespace
-                                   :externs ["resources/private/js/codemirror-externs.js"]
-                                   :pretty-print true}}}})
+                                        ; for details on how it is run.
+            :test
+            {:source-path "test"
+             :compiler {:output-to "resources/private/js/unit-test.js"
+                        :optimizations :whitespace
+                        :externs ["resources/private/js/codemirror-externs.js"]
+                        :pretty-print true}}}})
